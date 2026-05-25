ad4698852d133f7ab57e08335fce778d48cbe97e
Sennya <krkk970717@gmail.com>
2026-05-26 06:17:03 +0800

    Fix: 修改報錯邏輯，使try-except能捕獲拋出的異常

--- module/raise_tool.py
+++ module/raise_tool.py
@@ -1,8 +1,28 @@
 import sys
 from types import TracebackType
 
+is_silenced = False
+
+def silent_excepthook(exctype, value, traceback):
+    global is_silenced
+    if is_silenced:
+        pass
+    else:
+        sys.__excepthook__(exctype, value, traceback)
+    
+sys.excepthook = silent_excepthook
+
+def silent_raise():
+    global is_silenced
+    is_silenced = True
+    raise
+
 def clean_raise(exception = None):
+    except_class = exception.__class__
+    print(except_class)
+    
     if exception is None: exception = RuntimeError('No active exception to reraise')
+    exception.__traceback__ = None
     try:
         frame = sys._getframe(2)
     except ValueError:
@@ -22,6 +42,6 @@ def clean_raise(exception = None):
             tb_lasti = f.f_lasti,
             tb_lineno = f.f_lineno
         )
-        
+    
     sys.__excepthook__(type(exception), exception, traceback)
-    sys.exit(1)
+    silent_raise()
